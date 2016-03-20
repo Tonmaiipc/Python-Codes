f05a0b38a54f46d68cf76e2742ac208675f1f77e
Tonmaii <Tonmaiipc@users.noreply.github.com>
2016-03-20 12:40:17 +0700

    Update and rename TowerOfHanoi13Lines.py to TowerOfHano7Lines.py

--- TowerOfHano7Lines.py
+++ TowerOfHano7Lines.py
@@ -1,13 +1,7 @@
 max = int(raw_input()); moves = {}
 def ABC(max,n,i):
-	if (max%2==0)^(n%2==0) == True:
-		if i%3==0: return 'A'; 
-		if i%2==0: return 'C'; 
-		else: return 'B'
-	else:
-		if i%3==0: return 'A'; 
-		if i%2==0: return 'B';
-		else: return 'C'
+	if (max%2==0)^(n%2==0) == True: return 'BCA'[i%3]
+	else: return 'CBA'[i%3]
 for n in range(1,max+1):
 	for i in range(0, 2**(max-n)): moves[2**(n-1) + (2**n)*(i)] = str(n)+ABC(max,n,i)
 for i in range(1,2**max): print moves[i],
